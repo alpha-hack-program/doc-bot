54de0a22dc066409f0c6e11dff5edc103bcc4fab
cvicens <carlos.vicens.alonso@gmail.com>
2024-06-12 16:05:00 +0200

    controlling dtype and tensor paralellism

--- gitops/doc-bot/run.sh
+++ gitops/doc-bot/run.sh
@@ -4,4 +4,10 @@ ARGOCD_APP_NAME=doc-bot
 # Load environment variables
 . ../../bootstrap/.env
 
-helm template . --name-template ${ARGOCD_APP_NAME} --set dataScienceProjectNamespace=${DATA_SCIENCE_PROJECT_NAMESPACE} --set dataScienceProjectDisplayName=${DATA_SCIENCE_PROJECT_NAMESPACE} --include-crds
+helm template . --name-template ${ARGOCD_APP_NAME} \
+  --set dataScienceProjectNamespace=${DATA_SCIENCE_PROJECT_NAMESPACE} \
+  --set dataScienceProjectDisplayName=${DATA_SCIENCE_PROJECT_NAMESPACE} \
+  --set model.accelerator.productName="Tesla-T4" \
+  --set model.accelerator.min=2 \
+  --set model.accelerator.max=2 \
+  --include-crds
