b95842d62b872919baaebdb8d8d7a0a0cd597f0b
cvicens <carlos.vicens.alonso@gmail.com>
2024-07-17 20:35:33 +0200

    pl description updated

--- pipeline/load_documents_li.py
+++ pipeline/load_documents_li.py
@@ -1,6 +1,6 @@
 # DOCS: https://www.kubeflow.org/docs/components/pipelines/user-guides/components/ 
 
-# Pipeline to load documents from an S3 bucket into Milvus using llamaindex => li
+# Pipeline to load documents from an S3 bucket into Milvus using `llamaindex` => li
 
 import os
 import re
