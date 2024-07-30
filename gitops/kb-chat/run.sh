4568a5dbce39255aa6bc5ae436da7e1e79e21c52
cvicens <carlos.vicens.alonso@gmail.com>
2024-07-30 18:15:16 +0200

    new

--- gitops/kb-chat/run.sh
+++ gitops/kb-chat/run.sh
@@ -5,7 +5,5 @@ ARGOCD_APP_NAME=vllm-mistral-7b
 DATA_SCIENCE_PROJECT_NAMESPACE="vllm-mistral-7b-instruct-v0.2"
 
 helm template . --name-template ${ARGOCD_APP_NAME} \
-  --set instanceName="vllm-mistral-7b" \
-  --set dataScienceProjectNamespace=${DATA_SCIENCE_PROJECT_NAMESPACE} \
-  --set dataScienceProjectDisplayName=${DATA_SCIENCE_PROJECT_NAMESPACE} \
+  --set namespace=${DATA_SCIENCE_PROJECT_NAMESPACE} \
   --include-crds
