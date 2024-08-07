2591f3895300abcb926ccf4e20baa56a0d02ce42
cvicens <carlos.vicens.alonso@gmail.com>
2024-08-07 16:18:10 +0200

    two secrets one for bc and another one for tekton

--- bootstrap/create-secrets.sh
+++ bootstrap/create-secrets.sh
@@ -2,43 +2,74 @@
 
 . .env
 
-# Environment variables
-GIT_PAT_SECRET_NAME="git-pat-secret"
-GIT_BASE="https://gitlab.cm.jccm.es"
-# GIT_URL="${GIT_BASE}/inteligencia-artificial/geneva/geneva-ocp.git"
-GIT_USERNAME="gitlab"
+if [ -z "${GIT_BASE}" ]; then
+    echo "You should provide a git base url or add it to .env file"
+    exit 1
+fi
 
-NAMESPACE_STATUS=$(kubectl get namespace/${DATA_SCIENCE_PROJECT_NAMESPACE} -o jsonpath='{.status.phase}')
+# Git username
+echo "Username for ${GIT_BASE}: " && read GIT_USERNAME
+if [ -z "${GIT_USERNAME}" ]; then
+    echo "You should provide a username for ${GIT_BASE}"
+    exit 1
+fi
+
+# Create a secret for BuildConfig
+BC_GIT_PAT_SECRET_NAME="git-pat-bc-secret"
+
+# Check if the namespace exists
+NAMESPACE_STATUS=$(oc get namespace/${DATA_SCIENCE_PROJECT_NAMESPACE} -o jsonpath='{.status.phase}')
 if [ "${NAMESPACE_STATUS}" == *"Active"* ]; then
     echo "Wait until ArgoCD has create ns ${DATA_SCIENCE_PROJECT_NAMESPACE} or create it manually"
     exit 1
 fi
 
-echo "PAT for ${GIT_BASE}: " && read -s GIT_PAT
-if [ -z "${GIT_BASE}" ]; then
+echo "PAT for BC for ${GIT_BASE}: " && read -s BC_GIT_PAT
+if [ -z "${BC_GIT_PAT}" ]; then
     echo "You should provide a PAT for ${GIT_BASE}"
     exit 1
 fi
 
-cat <<EOF | kubectl apply -f -
+cat <<EOF | oc apply -f -
 apiVersion: v1
 kind: Secret
 metadata:
-  name: ${GIT_PAT_SECRET_NAME}
+  name: ${BC_GIT_PAT_SECRET_NAME}
+  namespace: ${DATA_SCIENCE_PROJECT_NAMESPACE}
+type: kubernetes.io/basic-auth
+stringData:
+  username: ${GIT_USERNAME}
+  password: ${BC_GIT_PAT}
+EOF
+
+# Create a secret for Tekton
+TK_GIT_PAT_SECRET_NAME="git-pat-tk-secret"
+
+echo "PAT for Tekton for ${GIT_BASE}: " && read -s TK_GIT_PAT
+if [ -z "${TK_GIT_PAT}" ]; then
+    echo "You should provide a PAT for ${GIT_BASE}"
+    exit 1
+fi
+
+cat <<EOF | oc apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ${TK_GIT_PAT_SECRET_NAME}
   namespace: ${DATA_SCIENCE_PROJECT_NAMESPACE}
 type: kubernetes.io/basic-auth
 stringData:
   user.name: ${GIT_USERNAME}
   user.email: "${GIT_USERNAME}@example.com"
   username: ${GIT_USERNAME}
-  password: ${GIT_PAT}
+  password: ${TK_GIT_PAT}
 EOF
 
-kubectl annotate --overwrite -n ${DATA_SCIENCE_PROJECT_NAMESPACE} secret ${GIT_PAT_SECRET_NAME} \
+oc annotate --overwrite -n ${DATA_SCIENCE_PROJECT_NAMESPACE} secret ${TK_GIT_PAT_SECRET_NAME} \
   "tekton.dev/git-0=${GIT_BASE}"
 
-kubectl annotate --overwrite -n ${DATA_SCIENCE_PROJECT_NAMESPACE} secret ${GIT_PAT_SECRET_NAME} \
+oc annotate --overwrite -n ${DATA_SCIENCE_PROJECT_NAMESPACE} secret ${TK_GIT_PAT_SECRET_NAME} \
   "build.openshift.io/source-secret-match-uri-1=${GIT_BASE}/*"
 
-kubectl label --overwrite -n ${DATA_SCIENCE_PROJECT_NAMESPACE} secret ${GIT_PAT_SECRET_NAME} \
-  credential.sync.jenkins.openshift.io=true
+oc label --overwrite -n ${DATA_SCIENCE_PROJECT_NAMESPACE} secret ${TK_GIT_PAT_SECRET_NAME} \
+  "credential.sync.jenkins.openshift.io=true"
